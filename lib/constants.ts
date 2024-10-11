d26d6d782d474fcc17fdae37f62a2b385518a09d
Chris Gambrell <christopher@gambrell.info>
2024-10-10 21:01:59 -0400

    Add registeration

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+import { CommandIcon } from 'lucide-react'
+
+// TODO: Real app name and icon
+export const MOCK_APP_ICON = CommandIcon
+export const MOCK_APP_NAME = 'Acme INc'
+
+// TODO: Real testimonial
+export const MOCK_TESTIMONIAL = {
+	name: 'Sofia Davis',
+	quote: 'This library has saved me countless hours of work and helped me deliver stunning designs to my clients faster than ever before.',
+}
+
+export const PLACEHOLDER_IMAGE = 'https://ui.shadcn.com/placeholder.svg'
