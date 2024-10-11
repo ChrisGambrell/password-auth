9d7941d98d43791fc914cc08b18ac1ff02a0f9a6
Chris Gambrell <christopher@gambrell.info>
2024-10-11 14:34:31 -0400

    Add ability to subscribe and manage subscriptions

--- app/(protected)/settings/(general)/client.tsx
+++ app/(protected)/settings/(general)/client.tsx
@@ -1,17 +1,104 @@
 'use client'
 
+import { checkoutWithStripe, createStripePortal } from '@/actions/stripe'
 import { updateName } from '@/actions/user'
 import { ActionButton } from '@/components/action-button'
 import { FormInput } from '@/components/form-input'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { getStripe } from '@/lib/stripe/client'
 import { AuthUser } from '@/lib/utils'
+import { getErrorRedirect } from '@cgambrell/utils'
+import { Price, Prisma } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+import { FormEvent, useState } from 'react'
 import { useFormState } from 'react-dom'
 
-export function ClientPage({ user }: { user: AuthUser }) {
+export function ClientPage({ prices, user }: { prices: Prisma.PriceGetPayload<{ include: { product: true } }>[]; user: AuthUser }) {
+	const router = useRouter()
+	const [interval, setInterval] = useState<string>('month')
+	const [loading, setLoading] = useState(false)
 	const [state, action] = useFormState(updateName, null)
+
+	async function handleCheckout(priceId: Price['id'], e: FormEvent) {
+		e.preventDefault()
+		setLoading(true)
+
+		const res = await checkoutWithStripe(priceId, '/settings')
+		if (res.errorRedirect) return router.push(res.errorRedirect)
+		else if (!res.sessionId) return router.push(getErrorRedirect('/settings', 'Unable to create checkout session.'))
+
+		const stripe = await getStripe()
+		stripe?.redirectToCheckout({ sessionId: res.sessionId })
+	}
 
 	return (
 		<div className='grid gap-6'>
+			<Card>
+				<CardHeader>
+					<CardTitle>Your plan</CardTitle>
+					<CardDescription>
+						{user.subscriptions.length > 0 ? (
+							<>
+								You are subscribed to the{' '}
+								<span className='font-bold underline'>{user.subscriptions[0].price?.product?.name}</span> plan.
+							</>
+						) : (
+							'You are not currently subscribed to any plan.'
+						)}
+					</CardDescription>
+				</CardHeader>
+				{user.subscriptions.length === 0 ? (
+					<CardContent>
+						<div className='grid gap-8'>
+							<Separator />
+							<Tabs value={interval} onValueChange={setInterval}>
+								<div className='flex justify-center'>
+									<TabsList>
+										<TabsTrigger value='month'>Monthly</TabsTrigger>
+										<TabsTrigger value='year'>Yearly</TabsTrigger>
+									</TabsList>
+								</div>
+								<div className='mt-4 grid grid-cols-3 gap-4'>
+									{prices
+										.filter((price) => price.interval === interval)
+										.map((price) => (
+											<Card key={price.id}>
+												<CardHeader>
+													<CardTitle>{price.product?.name}</CardTitle>
+													<CardDescription>
+														${Number(price.unit_amount) / 100} / {price.interval}
+													</CardDescription>
+												</CardHeader>
+												<CardContent>
+													<ul className='list-disc pl-4'>
+														{price.product?.features.map((feature) => (
+															<li key={feature}>{feature}</li>
+														))}
+													</ul>
+												</CardContent>
+												<CardFooter>
+													<form onSubmit={handleCheckout.bind(null, price.id)}>
+														<ActionButton className='w-full' loading={loading}>
+															Subscribe
+														</ActionButton>
+													</form>
+												</CardFooter>
+											</Card>
+										))}
+								</div>
+							</Tabs>
+						</div>
+					</CardContent>
+				) : (
+					<CardFooter className='border-t px-6 py-4'>
+						<form action={createStripePortal.bind(null, '/settings')}>
+							<ActionButton>Manage subscription</ActionButton>
+						</form>
+					</CardFooter>
+				)}
+			</Card>
 			<form action={action}>
 				<Card>
 					<CardHeader>
--- app/(protected)/settings/(general)/page.tsx
+++ app/(protected)/settings/(general)/page.tsx
@@ -1,7 +1,10 @@
 import { auth } from '@/lib/auth'
+import prisma from '@/lib/db'
 import { ClientPage } from './client'
 
 export default async function SettingsPage() {
 	const user = await auth()
-	return <ClientPage user={user} />
+	const prices = await prisma.price.findMany({ where: { active: true, product: { active: true } }, include: { product: true } })
+
+	return <ClientPage prices={prices} user={user} />
 }
