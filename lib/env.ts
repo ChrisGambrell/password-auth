8cf70f4164efaa743af260608d1528aa301cec20
Chris Gambrell <christopher@gambrell.info>
2024-10-10 20:45:41 -0400

    Add prisma

--- lib/env.ts
+++ lib/env.ts
@@ -4,9 +4,13 @@ import { z } from 'zod'
 export const env = createEnv({
 	server: {
 		AUTH_SECRET: z.string().min(1),
+
+		DATABASE_URL: z.string().url(),
 	},
 	client: {},
 	runtimeEnv: {
 		AUTH_SECRET: process.env.AUTH_SECRET,
+
+		DATABASE_URL: process.env.DATABASE_URL,
 	},
 })
